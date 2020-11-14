3debe6d55ab1608a0e2c099c7adb2672fb479719
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-11-14 18:34:04 +0100

    Changed script for server

--- script.bash
+++ script.bash
@@ -11,16 +11,16 @@ folder=$(date +'%Y-%m-%d__%H.%M.%S')
 mkdir -m 777 -p "logs/${folder}"
 mkdir -m 777 -p "logs/${folder}/extra"
 #
-# == Executing N Ingestion ([M] [N] [l/s] [table_name] [file_name_in_data_folder]) #TEMPERATURE_HalfGB_ns.csv
+# == Executing N Ingestion ([M] [N] [l/s] [table_name] [file_name_in_data_folder]) #TEMPERATURE_nodup.csv
 echo "Executing NDataIngestionTest.jar..."
-java -jar standalone/NDataIngestionTest.jar 200 200 $1 $2 TEMPERATURE_nodup.csv > "logs/${folder}/extra/out__N_ingestion.txt"
+java -jar standalone/NDataIngestionTest.jar 200 200 $1 $2 TEMPERATURE_HalfGB_ns.csv > "logs/${folder}/extra/out__N_ingestion.txt"
 rm -r logs/2020*_200
 echo "Ingestion completed!"
 echo ""
 #
-# == Executing Ingestion Part ([l/s] [dbName] [data_file_name] [log_folder] [inmem/tsi])
+# == Executing Ingestion Part ([l/s] [dbName] [data_file_name] [log_folder] [inmem/tsi]) #TEMPERATURE_nodup.csv
 echo "Executing IngestionMixed.jar ..."
-nohup java -jar standalone/IngestionMixed.jar $1 $2 TEMPERATURE_nodup.csv $folder $3 > "logs/${folder}/extra/out__ingestion.txt" 2> "logs/${folder}/extra/err__ingestion.txt" &
+nohup java -jar standalone/IngestionMixed.jar $1 $2 TEMPERATURE_1GB_ns.csv $folder $3 > "logs/${folder}/extra/out__ingestion.txt" 2> "logs/${folder}/extra/err__ingestion.txt" &
 #
 # == Executing Querying Part ([l/s] [dbName] [log_folder] [inmem/tsi])
 echo "Executing QueryingMixed.jar ..."
@@ -28,7 +28,7 @@ nohup java -jar standalone/QueryingMixed.jar $1 $2 $folder $3 > "logs/${folder}/
 #
 # == Waiting 2 hours
 echo "Sleeping 2 hours..."
-sleep 30s
+sleep 2h
 #
 # == Stopping the processes
 echo ""
