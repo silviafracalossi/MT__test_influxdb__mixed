9e686a532b918b953705e37d089ddfafb6f576ef
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-11-14 19:13:29 +0100

    Enhanced script to fit local and server characteristics

--- script.bash
+++ script.bash
@@ -11,16 +11,27 @@ folder=$(date +'%Y-%m-%d__%H.%M.%S')
 mkdir -m 777 -p "logs/${folder}"
 mkdir -m 777 -p "logs/${folder}/extra"
 #
-# == Executing N Ingestion ([M] [N] [l/s] [table_name] [file_name_in_data_folder]) #TEMPERATURE_nodup.csv
+# == Checking the data files to be used
+local="l"
+half_data="TEMPERATURE_HalfGB_ns.csv"
+full_data="TEMPERATURE_1GB_ns.csv"
+sleep_time="2h"
+if [ "$1" = "$local" ]; then
+    half_data="TEMPERATURE_nodup.csv"
+    full_data="TEMPERATURE_nodup.csv"
+    sleep_time="1m"
+fi
+#
+# == Executing N Ingestion ([M] [N] [l/s] [table_name] [file_name_in_data_folder])
 echo "Executing NDataIngestionTest.jar..."
-java -jar standalone/NDataIngestionTest.jar 200 200 $1 $2 TEMPERATURE_HalfGB_ns.csv > "logs/${folder}/extra/out__N_ingestion.txt"
-rm -r logs/2020*_200
+java -jar standalone/NDataIngestionTest.jar 300 300 $1 $2 ${half_data} > "logs/${folder}/extra/out__N_ingestion.txt"
+rm -r logs/2020*_300
 echo "Ingestion completed!"
 echo ""
 #
-# == Executing Ingestion Part ([l/s] [dbName] [data_file_name] [log_folder] [inmem/tsi]) #TEMPERATURE_nodup.csv
+# == Executing Ingestion Part ([l/s] [dbName] [data_file_name] [log_folder] [inmem/tsi])
 echo "Executing IngestionMixed.jar ..."
-nohup java -jar standalone/IngestionMixed.jar $1 $2 TEMPERATURE_1GB_ns.csv $folder $3 > "logs/${folder}/extra/out__ingestion.txt" 2> "logs/${folder}/extra/err__ingestion.txt" &
+nohup java -jar standalone/IngestionMixed.jar $1 $2 ${full_data} $folder $3 > "logs/${folder}/extra/out__ingestion.txt" 2> "logs/${folder}/extra/err__ingestion.txt" &
 #
 # == Executing Querying Part ([l/s] [dbName] [log_folder] [inmem/tsi])
 echo "Executing QueryingMixed.jar ..."
@@ -28,7 +39,7 @@ nohup java -jar standalone/QueryingMixed.jar $1 $2 $folder $3 > "logs/${folder}/
 #
 # == Waiting 2 hours
 echo "Sleeping 2 hours..."
-sleep 2h
+sleep ${sleep_time}
 #
 # == Stopping the processes
 echo ""
